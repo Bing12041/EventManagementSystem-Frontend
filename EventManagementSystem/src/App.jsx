3bbba986602db216a6f11907fb4dadc7096dbe04
Bingrun Zheng <bingrun1204@gmail.com>
2025-01-25 02:33:06 -0500

    fixed implement

--- EventManagementSystem/src/App.jsx
+++ EventManagementSystem/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './app.css';
+import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
